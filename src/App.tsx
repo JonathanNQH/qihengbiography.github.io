68d6dc437561570d1b0352d38c2d145c92afc9d9
[dyad] <ngqiheng@hotmail.com>
2025-11-27 15:24:21 +0800

    [dyad] wrote 2 file(s), deleted 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import Biography from "./pages/Biography"; // Import the new Biography page
+import Biography from "./pages/Biography"; // Import the Biography page
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -16,8 +15,7 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/biography" element={<Biography />} /> {/* Add the new route */}
+          <Route path="/" element={<Biography />} /> {/* Make Biography the default route */}
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
