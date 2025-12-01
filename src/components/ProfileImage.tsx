96957553d528586036732cdc15c514ed1b3b7d5a
[dyad] <ngqiheng@hotmail.com>
2025-12-02 01:42:39 +0800

    [dyad] wrote 1 file(s)

--- src/components/ProfileImage.tsx
+++ src/components/ProfileImage.tsx
@@ -11,7 +11,7 @@ interface ProfileImageProps {
 
 const ProfileImage: React.FC<ProfileImageProps> = ({ src, alt, className }) => {
   return (
-    <div className={cn("relative w-64 h-64 md:w-80 md:h-80 flex-shrink-0 rounded-lg overflow-hidden shadow-lg border-4 border-background", className)}>
+    <div className={cn("relative w-full aspect-square flex-shrink-0 rounded-lg overflow-hidden shadow-lg border-4 border-background", className)}>
       <img
         src={src}
         alt={alt}
